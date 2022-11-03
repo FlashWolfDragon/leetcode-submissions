509887d7e2474d117550bfddef1cd51c579d100b
Flash526 <dom526@gmail.com>
2022-11-03 13:32:05 -0400

    1544 make the string great

---
+++ problems/1544.make-the-string-great.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1544 lang=python3
+#
+# [1544] Make The String Great
+#
+
+# @lc code=start
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c.swapcase():
+                stack.pop()
+            else: 
+                stack.append(c)
+        return ''.join(stack)
+        
+# @lc code=end
+
