8ebdfbb14b45aae2e320915df64ba41d1969e002
Flash526 <dom526@gmail.com>
2022-11-03 15:13:34 -0400

    496 optimized next greater element i

--- problems/496.next-greater-element-i.py
+++ problems/496.next-greater-element-i.py
@@ -5,22 +5,27 @@
 #
 
 # @lc code=start
+from collections import defaultdict
+
 class Solution:
     def nextGreaterElement(self, nums1, nums2):
+        stack = []
+        pairs = defaultdict(int)
+        for x in nums2:
+            while stack and stack[-1] < x:
+                pairs[stack.pop()] = x
+            stack.append(x)
+
         res = []
-        for x in nums1:
-            after = found = False
-            for y in nums2:
-                if (not after and x == y):
-                    # find next largest element
-                    after = True
-                    pass
-                if (after and not found and y > x):
-                    res.append(y)
-                    found = True
-            if not found:    
+        for y in nums1:
+            curr = pairs[y]
+            if curr:
+                res.append(curr)
+            else:
                 res.append(-1)
+
         return res
-        
 # @lc code=end
+# print(Solution().nextGreaterElement([4,1,2], [1,3,4,2]))
+print(Solution().nextGreaterElement([1,3,5,2,4], [6,5,4,3,2,1,7]))
 
